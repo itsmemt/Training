aae49895a5d0eed15d404b3c27461deb857dbaa6
itsmemt <itsmeanimatormt@gmail.com>
2022-06-08 10:58:49 +0530

    f

--- Training/settings.py
+++ Training/settings.py
@@ -14,8 +14,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -121,9 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# LOGIN_REDIRECT_URL='/afterlogin'
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[STATIC_DIR,'static']
-
-LOGIN_REDIRECT_URL='/afterlogin'
+STATICFILES_DIRS=[BASE_DIR,'static']
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_ROOT=os.path.join(BASE_DIR,'static')
